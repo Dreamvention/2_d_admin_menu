7ae0a88b0c96ecbb4b2461ed5ed6e92ea4c6f512
Dmytro Sirjachenko <php6.drvn@gmail.com>
2017-06-08 22:56:07 +0300

    add language EN-GB

---
+++ admin/language/en-gb/extension/module/d_admin_menu.php
@@ -0,0 +1,11 @@
+<?php
+
+$_['heading_title']             	= '<span style="color:#449DD0; font-weight:bold">Admin Menu</span><span style="font-size:12px; color:#999"> by <a href="http://www.opencart.com/index.php?route=extension/extension&filter_username=Dreamvention" style="font-size:1em; color:#999" target="_blank">Dreamvention</a></span>';
+$_['heading_title_main']       		= 'Admin Menu';
+$_['text_modules']                  = 'Modules';
+$_['text_edit']                 	= 'Edit Admin Menu';
+
+$_['text_menu']             		= 'Menu';
+$_['text_settings']         		= 'Settings';
+$_['text_design']         			= 'Design';
+$_['text_menu_structure']     		= 'Menu Structure';
